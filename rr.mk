8c24237f50d1d06a666856964dde1139cc182b03
Ben Edmunds <Tigger2014@users.noreply.github.com>
2018-08-24 05:21:37 +0100

    switch to rr

--- rr.mk
+++ rr.mk
@@ -1,5 +1,5 @@
 # Inherit from common
-$(call inherit-product, device/samsung/gte-common/lineage.mk)
+$(call inherit-product, device/samsung/gte-common/rr.mk)
 
 $(call inherit-product, device/samsung/gt510wifi/device.mk)
 
@@ -9,7 +9,7 @@ TARGET_SCREEN_WIDTH := 768
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gt510wifi
-PRODUCT_NAME := lineage_gt510wifi
+PRODUCT_NAME := rr_gt510wifi
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T550
 PRODUCT_MANUFACTURER := samsung
